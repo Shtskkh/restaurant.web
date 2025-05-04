08dd4a98250455e3be4c9932dd7d0cfbb73a07fe
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-04 20:45:19 +0700

    Добавлен MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-function App() {
+import {ThemeProvider} from "@emotion/react";
+import {createTheme, CssBaseline} from "@mui/material";
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+function App() {
     return (
-        <>
-        </>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+        </ThemeProvider>
     )
 }
 
