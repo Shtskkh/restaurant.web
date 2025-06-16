1e638ca4222f7a8c98370b5341d0bdadfad4ca71
Alexey Shatskikh <shtskkh@gmail.com>
2025-06-16 13:52:42 +0700

    title для сотрудников

--- src/routes/_auth/staff/index.tsx
+++ src/routes/_auth/staff/index.tsx
@@ -2,5 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import StaffIndexPage from "../../../pages/StaffPages/StaffIndexPage.tsx";
 
 export const Route = createFileRoute("/_auth/staff/")({
+  head: () => ({
+    meta: [{ title: "Сотрудники" }],
+  }),
   component: StaffIndexPage,
 });
